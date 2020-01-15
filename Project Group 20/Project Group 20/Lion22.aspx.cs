b70b3f4d5e84bbc9a93ec414a2a42e6798c5448b
ahmadsh3lan <gnaiemahmad99@gmail.com>
2020-01-15 19:06:21 +0200

    adding the 22 page on the story

---
+++ Project Group 20/Project Group 20/Lion22.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Media;
+
+namespace Dyslaxia
+{
+    public partial class Lion22 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void NextButton_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("Lion23.aspx");
+        }
+
+        protected void PlayButton_Command(object sender, CommandEventArgs e)
+        {
+            SoundPlayer splayer = new SoundPlayer(@"C:\Users\Wadeea Abukush\Dyslaxia\Dyslaxia\Sounds\22.wav");
+            splayer.Play();
+        }
+
+        protected void PrevButtun_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("Lion21.aspx");
+        }
+    }
+}
