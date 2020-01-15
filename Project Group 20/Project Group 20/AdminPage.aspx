bf7cb18087ec3e677f26a1db8fc976cfd78f28e4
AltorBa14 AltorBa14 <altorba14@gmail.com>
2020-01-15 16:32:22 +0000

    Adding Admin Page

---
+++ Project Group 20/Project Group 20/AdminPage.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AdminPage.aspx.cs" Inherits="Dyslaxia.AdminPage" %>
+<link href="StyleSheet.css" rel="stylesheet" />
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Admin Page</h1>
+            <p>&nbsp;</p>
+            <p>
+                <asp:Button ID="KidsList" runat="server" Text="Kids List" OnCommand="KidsList_Command" />
+                <asp:Button ID="ParentList" runat="server" Text="Parents List" OnCommand="ParentList_Command" />
+                <asp:Button ID="RPList" runat="server" Text="Recover Password List" OnCommand="RPList_Command" />
+                <asp:Button ID="ContactUsMessages" runat="server" Text="Contact Us Messages" OnCommand="ContactUsMessages_Command" Width="212px" />
+                <asp:Button ID="WishListPage" runat="server" Text="Stories Wish List" OnCommand="WishListPage_Command" />
+                <asp:Button ID="FeedBackListPage" runat="server" Text="FeedBacks" OnCommand="FeedBackListPage_Command" />
+                <asp:Button ID="SendMessageToParents" runat="server" Text="Send Messages To Parents" OnCommand="SendMessageToParents_Command" Width="232px" />
+            </p>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Project Group 20/Project Group 20/AdminPage.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Dyslaxia
+{
+    public partial class AdminPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void KidsList_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("KidsListPage.aspx");
+        }
+
+        protected void ParentList_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("ParentsListPage.aspx");
+        }
+
+        protected void ContactUsMessages_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("ContactUsList.aspx");
+        }
+
+        protected void WishListPage_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("WishListDisplay.aspx");
+        }
+
+        protected void SendMessageToParents_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("SendMessageToParents.aspx");
+        }
+
+        protected void FeedBackListPage_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("FeedBacksList.aspx");
+        }
+
+        protected void RPList_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("RecoverPasswordList.aspx");
+        }
+    }
+}
