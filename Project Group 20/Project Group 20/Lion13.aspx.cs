9697cf2161d0e76d96c3eb99257d208a2da6f45f
ahmadsh3lan <gnaiemahmad99@gmail.com>
2020-01-15 19:00:39 +0200

    adding the 13 page on the story

---
+++ Project Group 20/Project Group 20/Lion13.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Dyslaxia
+{
+    public partial class Lion13 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void NextButton_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("Lion14.aspx");
+        }
+
+        protected void PrevButtun_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("Lion12.aspx");
+        }
+    }
+}
