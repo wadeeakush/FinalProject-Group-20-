34b15fcaa95c073ab1262bd0d2cc53fda76a5ecf
ahmadsh3lan <gnaiemahmad99@gmail.com>
2020-01-15 18:53:24 +0200

    adding the 4 page on the story

---
+++ Project Group 20/Project Group 20/Lion4.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Media;
+
+namespace Dyslaxia
+{
+    public partial class Lion4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void NextButton_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("Lion5.aspx");
+        }
+
+        protected void PlayButton_Command(object sender, CommandEventArgs e)
+        {
+            SoundPlayer splayer = new SoundPlayer(@"C:\Users\Wadeea Abukush\Dyslaxia\Dyslaxia\Sounds\4.wav");
+            splayer.Play();
+        }
+
+        protected void PrevButtun_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("Lion3.aspx");
+        }
+    }
+}
