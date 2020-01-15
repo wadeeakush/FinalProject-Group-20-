5ee84450c22e543d225d90bc36f57edc7bfad92d
ahmadsh3lan <gnaiemahmad99@gmail.com>
2020-01-15 18:55:35 +0200

    adding the 7 page on the story

---
+++ Project Group 20/Project Group 20/Lion7.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Dyslaxia
+{
+    public partial class Lion7 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void NextButton_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("Lion8.aspx");
+        }
+
+   
+
+        protected void PrevButtun_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("Lion6.aspx");
+        }
+    }
+}
