db8e6ec370a34a40e1dd192cbe2ee89644025cb6
wadeeakush <wadeeabukush@gmail.com>
2020-01-17 00:26:48 +0200

    Adding Parents Page

---
+++ Project Group 20/Project Group 20/ParentsPage.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ParentsPage.aspx.cs" Inherits="Dyslaxia.ParentsPage" %>
+<link href="StyleSheet.css" rel="stylesheet" />
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="SignUpMyKidBottun" runat="server" OnCommand="SignUpMyKidBottun_Command" Text="SignUp my kid" />
+            <br />
+            <asp:Button ID="StoryWishButton" runat="server" OnCommand="StoryWishButton_Command" Text="Wish Story" />
+            <br />
+            <asp:Button ID="MessagesFromAdmin" runat="server" Text="Messages From Admin" OnCommand="MessagesFromAdmin_Command" Width="210px" />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <asp:TextBox ID="FeedBack" runat="server" placeholder="Write Your FeedBack"></asp:TextBox>
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+            <asp:Button ID="SendFeedBack" runat="server" Text="Send" OnCommand="SendFeedBack_Command" />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Project Group 20/Project Group 20/ParentsPage.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Dyslaxia
+{
+    public partial class ParentsPage : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SignUpMyKidBottun_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("SignUpPageKid.aspx");
+        }
+
+        protected void StoryWishButton_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("WishStoryPage.aspx");
+        }
+
+        protected void SendFeedBack_Command(object sender, CommandEventArgs e)
+        {
+            string dat = "Insert into [FeedBacks](Feedback) Values('" + FeedBack.Text + "')";
+            SqlCommand comm = new SqlCommand(dat, con);
+            con.Open();
+            comm.ExecuteNonQuery();
+            con.Close();
+            Label1.Text = ("Your FeedBack Has Been Sent, Thank YOU");
+        }
+
+        protected void MessagesFromAdmin_Command(object sender, CommandEventArgs e)
+        {
+            Response.Redirect("MessagesFromAdmin.aspx");
+        }
+    }
+}
