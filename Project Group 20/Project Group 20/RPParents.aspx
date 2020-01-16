ea58ac8cc0e8a30d8c14fa40de6fdb87eff6b7bb
wadeeakush <wadeeabukush@gmail.com>
2020-01-17 00:29:19 +0200

    Adding Parents Recover Password Page

---
+++ Project Group 20/Project Group 20/RPParents.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RPParents.aspx.cs" Inherits="Dyslaxia.RPParents" %>
+<link href="StyleSheet.css" rel="stylesheet" />
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <h1>Recover Password</h1>
+            <br />
+            <asp:TextBox ID="UserNameRecover" runat="server" placeholder="UserName"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+            <br />
+            <asp:Button ID="PasswordRecButton" runat="server" Height="53px" Text="Send Password To Email" Width="311px" OnCommand="PasswordRecButton_Command1" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Project Group 20/Project Group 20/RPParents.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Dyslaxia
+{
+    public partial class RPParents : System.Web.UI.Page
+    {
+        private DataTable dt;
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void PasswordRecButton_Command1(object sender, CommandEventArgs e)
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT COUNT(*) From [Parents] where UserName='" + UserNameRecover.Text + "'", con);
+            
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                string dat = "Insert into [RPTable](UserName) Values('" + UserNameRecover.Text + "')";
+                SqlCommand comm = new SqlCommand(dat, con);
+                con.Open();
+                comm.ExecuteNonQuery();
+                con.Close();
+                Label1.Text = ("You Will Recive Email in 24 Hours on your Email With Your Password");
+            }
+            else
+            {
+                Label1.Text = ("You Not Exist");
+            }
+        }
+    }
+}
